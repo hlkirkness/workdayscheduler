d781838b6ed7a61c8660d8baac0b6d4dfc6c1658
hlkirkness <hlkirkness@gmail.com>
2021-10-09 12:26:46 -0400

    functions added

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function() {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    timeTracker();
+})
