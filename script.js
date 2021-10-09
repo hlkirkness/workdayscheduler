539ae2cbbae89f31e1c6e5c5d72116e4a73a7035
hlkirkness <hlkirkness@gmail.com>
2021-10-09 12:53:27 -0400

    local storage added var error resolved

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").html(todayDate);
+$("#currentDay").html(todaysDate);
 
 $(document).ready(function() {
     $(".saveBtn").on("click", function () {
@@ -28,5 +28,17 @@ $(document).ready(function() {
             }
         })
     }
+
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
     timeTracker();
 })
